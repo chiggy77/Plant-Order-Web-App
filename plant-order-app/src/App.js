8862650922601ed7399c66db0609332a5c528ad8
pawisweta <sweta.p2507@gmail.com>
2023-08-10 15:21:06 +0530

    Plant Order Project

---
+++ plant-order-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Cart from './Components/Cart';
+import AllRoutes from './Components/Routes/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
+
